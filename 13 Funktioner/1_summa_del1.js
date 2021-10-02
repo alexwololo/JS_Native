3961e74dde98e119845dd466874ab6027acaeceb
alexwololo <alexander.caspersson@gmail.com>
2021-10-02 23:53:52 +0200

    prepped module 13

---
+++ 13 Funktioner/1_summa_del1.js
@@ -0,0 +1,28 @@
+// Uppgift: Summa: Del 1
+// Instruktioner
+// Skapa en variabel, sum, och tilldela en funktion till denna.
+
+// Funktionen ska ta emot en array, som kan antas innehålla nummer. Funktionen ska också summera värdena i array:en, och returnera summan.
+
+// Koden…
+
+// let numbers = [1, 2, 3]
+// console.log(sum(numbers))
+// … eller console.log(sum([1, 2, 3])), ska göra så att “6” loggas till webbläsarens konsol, eftersom 1 + 2 + 3 = 6.
+
+// På samma sätt ska sum([1, 2, 3, 4]) returnera 10, eftersom 1 + 2 + 3 + 4 = 10.
+
+// Utgå gärna, inuti funktionen, från en variabel som tilldelas numret 0, för att sedan addera talen i array:en i en loop, till exempel så här:
+
+// let sum = function (a) {
+//   let result = 0
+
+//   for(...) {
+//     // ...
+//   }
+
+//   return result
+// }
+// Ladda upp index.js.
+
+// Ladda upp din lösning
---
+++ 13 Funktioner/2_genomsnitt 2.js
@@ -0,0 +1,21 @@
+// Uppgift: Genomsnitt
+// Instruktioner
+// Skapa en variabel, average, och tilldela en funktion till denna.
+
+// Funktionen ska ta emot en array, som kan antas innehålla nummer. Funktionen ska räkna ut genomsnittet på numren i array:en, och returnera detta.
+
+// Ett genomsnitt kan räknas ut genom att summera alla tal, och sedan använda division.
+
+// Koden…
+
+// let numbers = [1, 2, 3]
+// console.log(average(numbers))
+// … eller console.log(average([1, 2, 3])), ska göra så att “2” loggas till webbläsarens konsol, eftersom (1 + 2 + 3) / 3 = 2.
+
+// På samma sätt ska average([1, 2, 3, 4]) returnera 2.5, eftersom (1 + 2 + 3 + 4) / 4 = 2,5.
+
+// Om en tom array skickas in som ett argument, så ska null returneras. average([]) ska alltså returnera null. Använd gärna en if-sats i funktionen, för att hantera detta fall. Funktioner kan ha fler än en return-sats, och funktionen avslutas när den första körs.
+
+// Ladda upp index.js.
+
+// Ladda upp din lösning
